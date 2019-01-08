442ad5409ea63ed6e8c3409a198c395d5475b744
quynguyen1510 <quycao1510@gmail.com>
2019-01-08 17:06:54 +0700

    Finish add layout for signup

---
+++ rails/sample_app/app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 150 },format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
+end
