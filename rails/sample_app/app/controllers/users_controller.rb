60ceca1c2b84206312a6c9bd6ca8266205e960c1
quynguyen1510 <quycao1510@gmail.com>
2019-01-10 10:40:00 +0700

    Finish advanced_login, login with session

--- rails/sample_app/app/controllers/users_controller.rb
+++ rails/sample_app/app/controllers/users_controller.rb
@@ -1,12 +1,29 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show]
+  before_action :correct_user, only: [:show]
+
   def new
   	@user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])  
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update attributes(user_params)
+    # Handle a successful update.
+      flash[:success] = 'Profile updated'
+    else
+      render 'edit'
+    end
   end
 
   def create
   	@user = User.new(user_params)
   	if @user.save
-      flash[:success] = 'Welcome to my demo'
+      flash[:success] = 'Signup Complete'
   	  redirect_to login_path
   	else
   	  render 'new'
@@ -20,5 +37,21 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    #check user login when try to enter url the orther site without login
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_path
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      unless @user == current_user
+        flash[:danger] = "You not have access"
+        redirect_to(root_path) 
+      end
     end
 end
--- rails/sample_app/app/models/user.rb
+++ rails/sample_app/app/models/user.rb
@@ -1,9 +1,37 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   #downcase all email before save in database
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 50 }
+  validates :password, presence:true
   validates :email, presence: true, length: { maximum: 150 },format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
   # encode the password
   has_secure_password
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  #return random token
+  def new_token
+  	SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated? remember_token
+  	BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def remember user
+  	user.remember
+  	cookies.permanent.signed[:user_id] = user.id
+  	cookies.permanent[:remember_token] = user.remember_token
+  end
 end
